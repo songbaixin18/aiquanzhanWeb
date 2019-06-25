c3505fe4c72bd0b7ee34614c2a3e6b217bb40040
songbaixin <songbaixin@jiuqi.com.cn>
2019-06-25 11:43:08 +0800

    1

--- local_js/article.js
+++ local_js/article.js
@@ -1,6 +1,16 @@
+import { get } from "https";
+
 $("#header").load("../local_header.html");
 $("#right").load("../local_right.html");
 $("#footer").load("../local_footer.html");
-$(function(){
+const API = "https://qzapi.songbaixin.com"
+$(function () {
     $("#hostname").text(window.location.host);
+    let src = document.location.pathname.match(/\w+/g)[1];
+    $.ajax({
+        url: API + "/read_number/" + src,
+        success: function (result) {
+            $("#read_numder").html(result["read_number"]);
+        }
+    });
 })
